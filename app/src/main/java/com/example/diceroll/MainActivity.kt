8afe733c8614f05219d7e59fe02480eea5e74c1b
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-04 16:47:39 +0100

    Changed the text to a dice image, and made it set its image based on the random int we get from roll.

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -21,7 +22,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randInt = Random.nextInt(6)+1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.setText(randInt.toString())
+        val drawableResource = when (randInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice)
+        diceImage.setImageResource(drawableResource)
     }
 }
