4b21f1c862931bef02715b8ef1d6c0158f251bec
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-04 15:51:05 +0100

    Change the button's text via Kotlin code (the old way).

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.diceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //test
+        // The old findViewById technique
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = ("Roll The Dice")
+
     }
 }
